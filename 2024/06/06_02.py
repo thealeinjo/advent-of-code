a01e5e4bcbe1931906dd3e6dacbeb3f2d1896d8a
Thea John <thea.john@telekom.de>
2024-12-10 10:13:37 +0100

    add

--- 2024/06/06_02.py
+++ 2024/06/06_02.py
@@ -1,4 +1,4 @@
-with open('2024/06/input.txt', 'r') as file:
+with open('2024/06/input_x.txt', 'r') as file:
     lines = file.read().splitlines()
 
 # create a list of all lines and chars in lines
@@ -15,75 +15,99 @@ for i, line in enumerate(allLines):
             allLines[i][j] = '.'
             break
 
-places_ive_been = []
+places_ive_been = [(firstPosition, '^')]
 
 newcount = 0
+othercount = 0
+
+def check_if_ive_been_there_and_went_right(position, direction):
+    for place in places_ive_been:
+        if place[0] == position and place[1] == direction:
+            print(place)
+            global newcount
+            newcount += 1
+            print('new obstacle =' + str(position))
+            return True
 
 def go_up(position):
     while position[0] != 0 and allLines[position[0] - 1][position[1]] == '.':
         position = (position[0] - 1, position[1])
-        # check if I have been here before
-        if position in places_ive_been:
-            global newcount
-            newcount += 1
-        places_ive_been.append(position)
+        if check_if_ive_been_there_and_went_right(position, '^'):
+            global othercount
+            othercount += 1
+            return None
+        places_ive_been.append((position, '^'))
     return position
 
 def go_right(position):
     while position[1] != len(allLines[0]) - 1 and allLines[position[0]][position[1] + 1] == '.':
         position = (position[0], position[1] + 1)
-        if position in places_ive_been:
-            global newcount
-            newcount += 1
-        places_ive_been.append(position)
+        if check_if_ive_been_there_and_went_right(position, '>'):
+            global othercount
+            othercount += 1
+            return None
+        places_ive_been.append((position, '>'))
     return position
 
 def go_down(position):
     while position[0] != len(allLines) - 1 and allLines[position[0] + 1][position[1]] == '.':
         position = (position[0] + 1, position[1])
-        if position in places_ive_been:
-            global newcount
-            newcount += 1
-        places_ive_been.append(position)
+        if check_if_ive_been_there_and_went_right(position, 'v'):
+            global othercount
+            othercount += 1
+            return None
+        places_ive_been.append((position, 'v'))
     return position
 
 def go_left(position):
     while position[1] != 0 and allLines[position[0]][position[1] - 1] == '.':
         position = (position[0], position[1] - 1)
-        if position in places_ive_been:
-            global newcount
-            newcount += 1
-        places_ive_been.append(position)
+        if check_if_ive_been_there_and_went_right(position, '<'):
+            global othercount
+            othercount += 1
+            return None
+        places_ive_been.append((position, '<'))
     return position
 
-maxRounds = 150
+def go_around(allLines, position, go_up, go_right, go_down, go_left):
+    maxRounds = 150
 # while position doesn't reach end of line, reached end of allLines or reached -1 allLines or -1 line
-while position[1] != (len(allLines[1]) - 1) and position[0] != len(allLines) - 1 and position[0] != 0 and position[1] != 0 and maxRounds > 0:
-    position = go_up(position)
-    print(str(position) + ' next right')
-    if position[0] != 0:
-        position = go_right(position)
-        print(str(position) + ' next down')
-        if position[1] != len(allLines[1]) - 1:
-            position = go_down(position)
-            print(str(position) + ' next left')
-            if position[0] != len(allLines) - 1:
-                position = go_left(position)
-                print(str(position) + ' next up')
-    maxRounds -= 1
+    while position[1] != (len(allLines[1]) - 1) and position[0] != len(allLines) - 1 and position[0] != 0 and position[1] != 0 and maxRounds > 0:
+        position = go_up(position)
+        if position == None:
+            break
+    #print(str(position) + ' next right')
+        if position[0] != 0:
+            position = go_right(position)
+            if position == None:
+                break
+        #print(str(position) + ' next down')
+            if position[1] != len(allLines[1]) - 1:
+                position = go_down(position)
+                if position == None:
+                    break
+            #print(str(position) + ' next left')
+                if position[0] != len(allLines) - 1:
+                    position = go_left(position)
+                    if position == None:
+                        break
+                #print(str(position) + ' next up')
+        maxRounds -= 1
 
-print(len(allLines[0]))
-# make all places_ive_been unique
-places_ive_been = list(set(places_ive_been))
+for y, line in enumerate(allLines):
+    for x, char in enumerate(line):
+        new_grid = []
+        new_grid = allLines.copy()
+        #print(new_grid)
+        new_grid[y][x] = '#'
+        go_around(new_grid, position, go_up, go_right, go_down, go_left)
 
-# count all places_ive_been
-count = len(places_ive_been)
-print(count+1)
-print('newcount: ' + str(newcount))
+#print(len(allLines[0]))
+print(othercount)
 
 # replace all places_ive_been with 'O' in allLines
 for place in places_ive_been:
-    allLines[place[0]][place[1]] = 'O'
+    allLines[place[0][0]][place[0][1]] = place[1]
 
 allLines[firstPosition[0]][firstPosition[1]] = 'X'
 #allLines[23][129] = 'Y'
---
+++ 2024/10/10.py
@@ -0,0 +1,57 @@
+with open('2024/10/input.txt', 'r') as file:
+    lines = file.read().splitlines()
+
+# count all 0s and save their positions
+count = 0
+position_of_0 = []
+for x, line in enumerate(lines):
+    for y, char in enumerate(line):
+        if char == '0':
+            count += 1
+            position_of_0.append((x, y))
+
+def get_all_neighbors_and_its_value(x, y):
+    neighbors = []
+    if x - 1 >= 0:
+        neighbors.append((x - 1, y, lines[x - 1][y]))
+    if x + 1 < len(lines):
+        neighbors.append((x + 1, y, lines[x + 1][y]))
+    if y - 1 >= 0:
+        neighbors.append((x, y - 1, lines[x][y - 1]))
+    if y + 1 < len(lines[0]):
+        neighbors.append((x, y + 1, lines[x][y + 1]))
+    return neighbors
+
+
+def find_paths(x, y, current_num, max_tries):
+    if current_num == 9:
+        all_reached_9s.append((x, y))
+        return 1
+    elif max_tries <= 0:
+        return 0
+    neighbors = get_all_neighbors_and_its_value(x, y)
+    neighbors = list(filter(lambda neighbor: neighbor[2] == str(current_num + 1), neighbors))
+    if len(neighbors) == 0:
+        return 0
+    path_count = 0
+    for neighbor in neighbors:
+        #print('current_position:', neighbor)
+        path_count += find_paths(neighbor[0], neighbor[1], current_num + 1, max_tries - 1)
+    return path_count
+
+max_tries = 1000
+sum = 0
+for x, y in position_of_0:
+    current_num = 0
+    current_position = (x, y)
+    all_reached_9s = []
+    total_paths = find_paths(x, y, int(lines[x][y]), max_tries)
+    #print(f"Total paths found: {total_paths}")
+    # make all_reached_9s unique
+    all_reached_9s = list(set(all_reached_9s))
+    #print(len(all_reached_9s))
+    sum += len(all_reached_9s)
+
+print(sum)
+
+        
